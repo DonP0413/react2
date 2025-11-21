c39e90a0d480f9592a2d1477a4a5dfe7aee688af
Miguel <akmp2019@outlook.com>
2025-11-20 19:04:29 -0500

    delpoy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANTE: Esto debe coincidir con el nombre de tu repositorio en GitHub
+  base: "/react2/", 
 })
